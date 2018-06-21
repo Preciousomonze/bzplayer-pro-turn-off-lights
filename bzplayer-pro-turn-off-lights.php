48525aa16ae58e1e2aa100a1693b63db65021ea7
Omonzejele Precious <crispusomonze@gmail.com>
2018-06-22 00:38:02 +0100

    added plugin url

--- bzplayer-pro-turn-off-lights.php
+++ bzplayer-pro-turn-off-lights.php
@@ -4,7 +4,7 @@
  */
 /*
 Plugin Name: bzplayer-pro Turn Off the lights
-Plugin URI: https://github.com/preyous
+Plugin URI: https://github.com/preyous/bzplayer-pro-turn-off-lights
 Description: This plugin gives the "Turn off the lights" feature when your bzplayer-pro videos are set to play. Very light and should be compatible with recent Wordpress
 Version: 1.0.0
 Author: Precious Omonzejele
