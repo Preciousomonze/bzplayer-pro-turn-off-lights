12430c1b8b9811e7ccfb52a0ff775d3570560eda
Omonzejele Precious <crispusomonze@gmail.com>
2018-06-25 23:22:04 +0100

    i dont know what i pushed jare :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
  */
 var $ = jQuery;
 var bpzShadowId = "bpz-pk-shadow";
+
 //set the height before handle
 $(document).ready(function(){
     $("#"+bpzShadowId).css("height", $(document).height()).hide();
@@ -14,6 +15,22 @@ $(document).ready(function(){
  *@param int ticketId
  *@param string type, if its form type, does the necessary, else, bla, i'm tired jare.
  */
-function bpzPkTurnOffTheLights(element_id){
-	$(element)
+function bpzPkTurnOffTheLights(elementId){
+	var _btnId = $("#"+elementId);
+	//check if the shadow is already there
+	if($("#"+bpzShadowId).hasClass('is-showing')){//the lights are already off
+		//now check if the btn clicked has a class vjs-paused
+	}
+	else{//do the process
+		//set the z-index of the video player
+		_btnId.closest("video").css('z-index','1999');
+		$("#"+bpzShadowId).show();
+	}
 }
+
+$("button.vjs-button.vjs-control").click(function(){
+   //set an id attr for it
+   var theRand = Math.floor((Math.random() * 400) + 1);//gen random number for id
+  var theId = $(this).attr('id','bpz-btn-'+theRand);
+	bpzPkTurnOffTheLights(theId); 
+});
