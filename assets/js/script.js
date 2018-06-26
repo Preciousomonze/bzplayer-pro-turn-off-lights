59d8d9e476d37af1cba6ba0e2329d08056a25144
Omonzejele Precious <crispusomonze@gmail.com>
2018-06-26 22:53:19 +0100

    structuring js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,77 @@
-/* script to handle the lights off feature 
+/* script to handle the lights off feature, trying to see if it'll be necessary to detect if its an iframe, or directly a video
+ * Iframes are used when the video is from stuff like youtube, etc.
  * @author Precious Omonzejele <omonze@peepsipi.com>
  */
 var $ = jQuery;
-var bpzShadowId = "bpz-pk-shadow";
-
+var bzpshadowId = "bpz-pk-shadow";
+var bzpvidClass = '';//for the class
+var bzpvidzindex = '';//store the z-index
 //set the height before handle
 $(document).ready(function(){
-    $("#"+bpzShadowId).css("height", $(document).height()).hide();
-	$('body').prepend('<div id="'+bpzShadowId+'"></div>')
+    $("#"+bzpshadowId).css("height", $(document).height()).hide();
+	$('body').prepend('<div id="'+bzpshadowId+'"></div>')
 });
 
 /**
  * bring the light effect and do other stuff
  *@param int elementClass the video class
  */
-function bpzPkTurnOffTheLights(elementClass){
-	var _btnId = $("#"+elementId);
+function bzppkturnOffTheLights(elementClass){
+	alert("lights are off");
+	var _thevid = $("."+elementClass);
+	//set the z-index
+	_thevid.css('z-index','1999');
+	//show the div shadow
+	$('#'+bzpshadowId).fadeIn();	
 	//check if the shadow is already there
-	if($("#"+bpzShadowId).hasClass('is-showing')){//the lights are already off
+	if($("#"+bzpshadowId).hasClass('is-showing')){//the lights are already off
 		//now check if the btn clicked has a class vjs-paused
 	}
-	else{//do the process
-		//set the z-index of the video player
-		_btnId.closest("video").css('z-index','1999');
-		$("#"+bpzShadowId).show();
-	}
+
 }
 
 /**
  * turn of the light effect and do other stuff
  *@param int elementClass the video class
  */
-function bpzpkturnOnTheLights(elementClass){
-	
+function bzppkturnOnTheLights(elementClass){
+	alert("lights are on");
+	var _thevid = $("."+elementClass);
+	//set the z-index
+		_thevid.removeAttr('z-index',bzpvidzindex);	
+	//show the div shadow
+	$('#'+bzpshadowId).fadeOut();	
 }
-$("button.vjs-button.vjs-control").click(function(){
+//the button event ish
+function bzppkbtnEv(){
+	var _this = "button.vjs-button.vjs-control";
    //set an id attr for it
    var theRand = Math.floor((Math.random() * 400) + 1);//gen random number for id
-  var theId = $(this).attr('id','bpz-btn-'+theRand);
-	bpzPkTurnOffTheLights(theId); 
+   var oldId = $(_this).attr('id');
+   if(oldId == "")
+		var theId = $(_this).attr('id','bpz-btn-'+theRand);
+	else
+		var theId = oldId;
+	
+  bzpvidClass = "bzp-pk-vid-"+theId;
+  //check if video tag exists, if not, it's an iframe, then we check the video inside the iframe
+  //get the vid z-index
+  	bzpvidzindex = $(_this).closest(".video-js").css('z-index');
+	//create a unique css class
+	bzpvidClass = $(_this).closest(".video-js").addClass(bzpvidClass);
+}
+
+//for the big play btn
+$('.vjs-big-play-button').click(function(){
+	bzppkbtnEv();
 });
+//for the little play button
+$("button.vjs-button.vjs-control").click(function(){
+	bzppkbtnEv();
+});
+
+//now the pure js
+var bzpvidItself = document.getElementsByClassName(bzpvidClass);
+bzpvidItself.onplay = bzppkturnOffTheLights(bzpvidClass);
+bzpvidItself.onpause = bzppkturnOnTheLights(bzpvidClass);
+bzpvidItself.onended = bzppkturnOnTheLights(bzpvidClass);
